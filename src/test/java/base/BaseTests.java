5c811bf3b81b3654c8f9b4f2e9abf5ea9626ed96
Sara Ramadan <sara.ramadan@garment.io>
2025-04-12 04:00:10 +0200

    add testng and headless

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -2,9 +2,10 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.*;
 import pages.HomePage;
 
 import java.io.FileNotFoundException;
@@ -15,13 +16,39 @@ public class BaseTests {
 
     WebDriver driver;
     protected HomePage homePage;
+    public ChromeOptions chromeOptions;
+    public FirefoxOptions firefoxOptions;
 
+    @Parameters("browser")
     @BeforeClass
-    public void setUp()
+    public void setUp(@Optional("chrome")String browser)
     {
-        driver = new ChromeDriver();
+        setUpBrowser(browser);
         driver.manage().window().maximize();
         homePage = new HomePage(driver);
+    }
+    @Parameters("browser")
+    private void setUpBrowser(String browser)
+    {
+        if (browser.equalsIgnoreCase("chrome"))
+        {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+
+        }
+        else if (browser.equalsIgnoreCase("headlessChrome"))
+        {
+            chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless");
+            driver = new ChromeDriver(chromeOptions);
+        }
+        else if (browser.equalsIgnoreCase("headlessFirefox"))
+        {
+            firefoxOptions = new FirefoxOptions();
+            firefoxOptions.addArguments("--headless");
+            driver = new FirefoxDriver(firefoxOptions);
+        }
     }
 
     @BeforeMethod
